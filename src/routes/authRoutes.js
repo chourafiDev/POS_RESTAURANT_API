fa7efa16d8b325534e9e97bbe3d38b298cb5430d
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-24 18:23:46 +0100

    Change route post path

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+import { createUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/create-user", createUser);
 
 export default router;
