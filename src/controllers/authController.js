a10eb4ad64c23b21f5f1aeda8d7689a6de7c1a95
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-25 16:02:38 +0100

    Remove creat user action

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,45 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
-// @desc Auth use/set token
-// @route POST api/auth/register
+// @desc Create New User user/set token
+// @route POST api/auth/login
 // @access Public
-const register = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "test message" });
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if user already exists
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      message: "Logged in successfuly",
+      data: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        image: user.image,
+      },
+    });
+  } else {
+    res.status(400);
+    throw new Error(`Invalide email or password`);
+  }
 });
 
-export { register };
+// @desc Logout User
+// @route POST api/auth/logout
+// @access Public
+const logout = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({
+    message: "Logged out successfuly",
+  });
+});
+
+export { login, logout };
