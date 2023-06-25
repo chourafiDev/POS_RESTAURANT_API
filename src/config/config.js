716c1e1f8ff0eecb00cca52a09a91309b265752c
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-25 15:58:32 +0100

    Fix connection with mongoDB

--- src/config/config.js
+++ src/config/config.js
@@ -3,13 +3,20 @@ import "dotenv/config";
 
 const MONOG_USER = process.env.MONGO_USER;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_URL = `mongodb+srv://${MONOG_USER}:${MONGO_PASSWORD}@cluster0.mfgwrqv.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+const MONGO_URL = `mongodb+srv://${MONOG_USER}:${MONGO_PASSWORD}@cluster0.mfgwrqv.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority
+`;
 
-mongoose.Promise = Promise;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URL);
+    console.log(
+      `✔️ [MonogDb]: MongoDb Success Connected:  ${conn.connection.host}`
+    );
+  } catch (error) {
+    console.error(`❌[MonogDb]: MongoDb Faild Connected ${error}`);
+    process.exit(1);
+  }
+};
 
-mongoose.connect(MONGO_URL);
-mongoose.connection.on("error", (error) =>
-  console.log(`❌ [monogDb error]: ${error}`)
-);
-
-export const dbConnect = () => {};
+export default connectDB;
