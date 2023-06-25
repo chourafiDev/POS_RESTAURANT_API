f336f2d1eb8e29838a2f1438a646918c60d1df03
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-25 18:32:26 +0100

    Add getAllUsers api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,12 +3,14 @@ import {
   createUser,
   getUserProfile,
   updateUserProfile,
+  getAllUsers,
 } from "../controllers/userContoller.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create-user", createUser);
+router.post("/create-user", protect, createUser);
+router.get("/", protect, getAllUsers);
 router
   .route("/profile")
   .get(protect, getUserProfile)
