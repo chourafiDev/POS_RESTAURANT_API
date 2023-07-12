d448a9b7d4e814311b9566509a63ec6914ce53e5
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-12 15:24:10 +0100

    Add model category

---
+++ src/models/productModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Category is required"],
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trime: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trime: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+    },
+    options: {
+      type: [String],
+      required: [true, "Options is required"],
+    },
+    image: {
+      public_id: {
+        type: String,
+        required: [true, "Image is required"],
+      },
+      url: {
+        type: String,
+        required: [true, "Image is required"],
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Prodcut", productSchema);
+
+export default Product;
