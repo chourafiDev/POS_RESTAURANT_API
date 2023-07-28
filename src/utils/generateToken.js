420882c79c68379d423d0b065f236794c02d1769
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-28 16:33:36 +0100

    Add sameSite to none

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
