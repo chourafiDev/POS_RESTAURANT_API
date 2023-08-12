456c55606f277c1bd0addfc493764b3269c4e686
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-08-12 21:29:10 +0100

    Change image on upload

--- src/controllers/userContoller.js
+++ src/controllers/userContoller.js
@@ -89,24 +89,24 @@ const updateUser = asyncHandler(async (req, res, next) => {
     user.phone = phone || user.phone;
 
     //Update user image
-    // if (image !== null) {
-    //   const image_id = user.image.public_id;
+    if (image) {
+      const image_id = user.image.public_id;
 
-    //   if (image_id) {
-    //     //Delete previous image
-    //     await cloudinary.uploader.destroy(image_id);
-    //   }
+      if (image_id) {
+        //Delete previous image
+        await cloudinary.uploader.destroy(image_id);
+      }
 
-    //   //add new image
-    //   const result = await cloudinary.uploader.upload(image, {
-    //     folder: "pos_app/avatars",
-    //   });
+      //add new image
+      const result = await cloudinary.uploader.upload(image, {
+        folder: "pos_app/avatars",
+      });
 
-    //   user.image = {
-    //     public_id: result.public_id,
-    //     url: result.secure_url,
-    //   };
-    // }
+      user.image = {
+        public_id: result.public_id,
+        url: result.secure_url,
+      };
+    }
 
     await user.save();
 
