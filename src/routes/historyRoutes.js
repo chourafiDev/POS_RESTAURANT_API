eb54a01b0c5ec7a49f0bc527d0147c4dab0b5a49
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-08-22 15:01:11 +0100

    Add routes for delete many histories or one history

--- src/routes/historyRoutes.js
+++ src/routes/historyRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getAllhistories } from "../controllers/historiesController.js";
+import {
+  getAllhistories,
+  deleteHistory,
+  deleteHistories,
+} from "../controllers/historiesController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", protect, getAllhistories);
-// router.delete("/:id", protect, deleteProduct);
+router.delete("/:id", protect, deleteHistory);
+router.post("/", protect, deleteHistories);
 
 export default router;
