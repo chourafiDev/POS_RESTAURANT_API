2946375a00549084113464e811c806db54838adb
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-08-27 18:54:38 +0100

    Change api method from GET to POST in forgot password route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,7 @@ import {
   getUserProfile,
   login,
   logout,
+  forgotPassword,
 } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/profile", protect, getUserProfile);
+router.post("/forgot-password", forgotPassword);
 
 export default router;
