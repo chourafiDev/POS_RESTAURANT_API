83ac2f56edf49268345a0706788a63bff55b805c
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-02 19:53:50 +0100

    handle customer modal

---
+++ src/models/customerModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;
