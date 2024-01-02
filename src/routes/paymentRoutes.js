f776581fedd7aca3f3395fe94f245c460fc0b089
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-02 19:51:18 +0100

    refactor: add webhook route

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { stripeCheckoutSession } from "../controllers/paymentController.js";
+import {
+  stripeCheckoutSession,
+  webHookCheckout,
+} from "../controllers/paymentController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/create-checkout-session", protect, stripeCheckoutSession);
+router.post("/webhook", webHookCheckout);
 
 export default router;
