3fbf6f5d4855da81b80ed8fa24305e2b2f9da7d9
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-02 20:06:17 +0100

    refactor:add route for get my history

--- src/routes/historyRoutes.js
+++ src/routes/historyRoutes.js
@@ -3,12 +3,14 @@ import {
   getAllhistories,
   deleteHistory,
   deleteHistories,
+  getMyHistory,
 } from "../controllers/historiesController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", protect, getAllhistories);
+router.get("/me", protect, getMyHistory);
 router.delete("/:id", protect, deleteHistory);
 router.post("/", protect, deleteHistories);
 
