790f8cbdad20d0a7bbc2b5979bc45214955598dd
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-02 11:41:47 +0100

    refactor: display items from req body

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -6,19 +6,24 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 // @route POST api/payments/create-checkout-session
 // @access Privet
 const stripeCheckoutSession = asyncHandler(async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "Egg Salad Croissant",
-          },
-          unit_amount: 2000,
+  const { cartItems, userId } = req.body;
+
+  const items = cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.title,
+          images: [item.image],
         },
-        quantity: 1,
+        unit_amount: item.price * 100,
       },
-    ],
+      quantity: item.qty,
+    };
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: items,
     mode: "payment",
     success_url: `${process.env.CLIENT_URL}/en/menu`,
     cancel_url: `${process.env.CLIENT_URL}/en/menu`,
