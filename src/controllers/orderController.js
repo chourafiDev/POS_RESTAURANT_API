49a0f6542baef20cdad34be26d8291bf51990aa6
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-04 12:40:27 +0100

    refactor: get orders between two dates

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -4,11 +4,27 @@ import Order from "../models/orderModel.js";
 // @desc Get orders
 // @route GET api/orders
 // @access Privet
-const getOrders = asyncHandler(async (req, res, next) => {
-  const orders = await Order.find({}).select("-__v -updatedAt").populate({
-    path: "customerId",
-    select: "name email phone -_id",
-  });
+const getOrders = asyncHandler(async (req, res) => {
+  const startDate = req.query.startDate || "";
+  const endDate = req.query.endDate || "";
+
+  let generateQuery = {};
+
+  if (startDate != "" && endDate != "") {
+    const endDateFull = new Date(endDate);
+    endDateFull.setHours(23, 59, 59, 999); // Set to the end of the day
+
+    generateQuery = {
+      createdAt: { $gte: new Date(startDate), $lte: endDateFull },
+    };
+  }
+
+  const orders = await Order.find(generateQuery)
+    .select("-__v -updatedAt")
+    .populate({
+      path: "customerId",
+      select: "name email phone -_id",
+    });
 
   res.status(200).json(orders);
 });
